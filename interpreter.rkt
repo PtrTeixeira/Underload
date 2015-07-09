0931e0f488d54694b0064b16217c8000e93d8378
PtrTeixeira <ptrTeixeira@gmail.com>
2015-07-08 22:42:13 -0400

    Continuing to add tests
    Classifier tests and the beginnings of run tests

--- interpreter.rkt
+++ interpreter.rkt
@@ -2,8 +2,42 @@
 
 (provide (all-defined-out))
 
-;; Tokenize: String -> [List String]
+;; A Token is one of 
+;; - "(.*)"
+;; - ":"
+;; - "~"
+;; - "*"
+;; - "a"
+;; - "^"
+;; - "!"
+;; - "S"
+
+
+;; A Classification is one of 
+;; - 'push
+;; - 'concat
+;; - 'duplicate
+;; - 'swap
+;; - 'enclose
+;; - 'eval
+;; - 'drop
+;; - 'print
+
+;; A State is a (make-state [List Token] [List Token] String)
+(define-struct state (program stack output))
+
+;; tokenize: String -> [List Token]
 ;; Breaks the input string into tokens, as per underload.
 ;; Throws an error upon hitting an unrecognized token
 (define (tokenize str) 
   str)
+
+;; classify: Token -> Classification
+;; Classify each token by the action that is required
+(define (classify token)
+  token)
+
+;; run: State -> String
+;; Run the program from the currently defined program state
+(define (run state)
+  state)
--- tests.rkt
+++ tests.rkt
@@ -23,4 +23,36 @@
                  '("(he)" "(l)" ":" "(o)" "*" "*" "*" "S"))))
 
 
+(define classifier-tests 
+  (test-suite 
+   "Classifier Tests"
+   (check-equal? (classify "(hello)") 'push)
+   (check-equal? (classify "*") 'concat)
+   (check-equal? (classify ":") 'duplicate)
+   (check-equal? (classify "~") 'swap)
+   (check-equal? (classify "a") 'enclose)
+   (check-equal? (classify "^") 'eval)
+   (check-equal? (classify "!") 'drop)
+   (check-equal? (classify "S") 'print)))
+
+(define runner-tests
+  (test-suite
+   "Runner tests"
+   (check-equal? (run (make-state '() '() "")) "")
+   (check-equal? (run (make-state 
+                  '("(he)" "(l)" ":" "(o)" "*" "*" "*" "S")
+                  '() ""))
+                 "hello")
+   (check-exn exn:fail? 
+              (λ()(run 
+                   (make-state '("*")
+                               '()
+                               ""))))
+   (check-exn exn:fail?
+              (λ()(run (make-state '(":") '() ""))))
+   (check-exn exn:fail? 
+              (λ()(run (make-state '("!") '() ""))))))
+
+
 (run-tests tokenizer-tests)
+(run-tests classifier-tests)
