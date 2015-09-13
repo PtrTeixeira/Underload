e84d7d003350c2e6649b6413ce0d6f2cea2ab9a0
PtrTeixeira <ptrTeixeira@gmail.com>
2015-09-13 18:13:31 -0400

    Typed Racket
    Made the change over to Typed Racket, which was easier than I thought it
    would be. I correctly guessed the only sticky spot, and it wasn't that
    hard to fix. So typed. This will probably be the last major change.

--- interpreter.rkt
+++ interpreter.rkt
@@ -1,5 +1,4 @@
-#! /usr/bin/env racket
-#lang racket
+#lang typed/racket
 
 (provide interpret)
 ;; A Token is one of
@@ -11,6 +10,8 @@
 ;; - "^"
 ;; - "!"
 ;; - "S"
+(define-type Token String)
+
 
 
 ;; A Classification is one of
@@ -22,16 +23,30 @@
 ;; - 'eval
 ;; - 'drop
 ;; - 'print
+(define-type Classification 
+  (U 'push
+     'concat
+     'duplicate
+     'swap
+     'enclose
+     'eval
+     'drop
+     'print))
 
 ;; A State is a (make-state [List Token] [List String])
-(define-struct state (program stack) #:transparent)
+(define-struct state ([program : (Listof Token)]
+                      [stack : (Listof String)]) #:transparent)
+;; I don't like types having lowercase names.
+(define-type State state)
 
 ;; tokenize: String -> [List Token]
 ;; Breaks the input string into tokens, as per underload.
 ;; Throws an error upon hitting an unrecognized token
+(: tokenize (-> String (Listof Token)))
 (define (tokenize str)
   ;; take-token: String -> Token
   ;; Produce the first token present in the string
+  (: take-token (-> String Token))
   (define (take-token str)
     (let ((fst (substring str 0 1)))
       (cond [(or (string=? fst ":") (string=? fst "~")
@@ -43,6 +58,7 @@
   
   ;; take-push-token-helper: String String Int -> Token
   ;; Helper method to count off paren depth
+  (: take-push-token-helper (-> String String Integer Token))
   (define (take-push-token-helper str build depth)
   (cond [(zero? depth) build]
         [else 
@@ -60,11 +76,13 @@
 
   ;; take-push-token: String -> Token
   ;; Parse and return a push token
+  (: take-push-token (-> String Token))
   (define (take-push-token str)
     (take-push-token-helper (substring str 1) "(" 1))
 
   ;; drop-token: String -> String
   ;; Return the string, absent the first token present
+  (: drop-token (-> String Token String))
   (define (drop-token str token)
     (substring str (string-length token)))
 
@@ -74,6 +92,7 @@
 
 ;; classify: Token -> Classification
 ;; Classify each token by the action that is required
+(: classify (-> Token Classification))
 (define (classify token)
   (cond [(string=? token ":") 'duplicate]
         [(string=? token "~") 'swap]
@@ -86,8 +105,10 @@
 
 ;; print-state: State -> String
 ;; Do a print-out of the current state
+(: print-state (-> State String))
 (define (print-state state)
   ;; [List <String|Token>] -> String
+  (: prgm-string (-> (Listof String) String))
   (define (prgm-string lst)
     (if (empty? lst) ""
         (string-append (first lst) (prgm-string (rest lst)))))
@@ -98,6 +119,7 @@
 ;; Push: State -> State
 ;; Guarded: prgm non-empty
 ;; Push token from program to stack
+(: push (-> State State))
 (define (push state) 
   (let ((fst (substring 
               (first (state-program state)) 
@@ -108,6 +130,7 @@
 ;; Guarded: prgm non-empty
 ;; Throw error if trying to duplicate on empty stack
 ;; Duplicate top element of stack
+(: duplicate (-> State State))
 (define (duplicate state)
   (if (empty? (state-stack state)) 
       (error 
@@ -121,6 +144,7 @@
 ;; Guard: prgm non-empty
 ;; Throw error if fewer than two elements on the stack
 ;; Swap top two elements on the stack
+(: swap (-> State State))
 (define (swap state)
   (match (state-stack state)
     [(list-rest a b c) (make-state (rest (state-program state))
@@ -133,6 +157,7 @@
 ;; Guard: prgm non-empty
 ;; Throw error if fewer than two elements on the stack
 ;; Concatenate the top two elements of the stack
+(: concat (-> State State))
 (define (concat state)
   (match (state-stack state)
     [(list-rest a b c) (make-state (rest (state-program state))
@@ -144,6 +169,7 @@
 ;; Guard: prgm non-empty
 ;; Throw error if stack is empty
 ;; Enclose top element of stack in parentheses
+(: enclose (-> State State))
 (define (enclose state)
   (match (state-stack state)
     [(list-rest a b) (make-state (rest (state-program state))
@@ -155,6 +181,7 @@
 ;; Guard : prgm non-empty
 ;; Throw error if stack is empty OR if popped item is not a valid program
 ;; Pop the first element from the stack onto the program
+(: eval (-> State State))
 (define (eval state)
   (if (empty? (state-stack state))
       (error (string-append "Attempted to evaluate on the empty stack: "
@@ -167,6 +194,7 @@
 ;; Guard: prgm non-empty
 ;; Throw error if stack is empty
 ;; Drop first element from the stack
+(: drop (-> State State))
 (define (drop state)
   (if (empty? (state-stack state))
       (error (string-append "Attempted to drop item from empty stack: "
@@ -179,6 +207,7 @@
 ;; Throw error if stack is empty
 ;; SIDE EFFECT: Will print to standard output
 ;; Print & drop first element from the stack
+(: prints (-> State State))
 (define (prints state)
   (if (empty? (state-stack state))
       (error (string-append "Attempted to print from empty stack: "
@@ -195,6 +224,7 @@
 ;; -> returned. This has to do with how the interpreter should behave 
 ;; -> on infinite programs. The returned value is the stack after the 
 ;; -> program terminates.
+(: run (-> State (Listof String)))
 (define (run state)
   (define prgm (state-program state))
   (define stck (state-stack state))
@@ -213,6 +243,7 @@
 
 ;; interpret: String -> [List String]
 ;; Run program from string input
+(: interpret (-> String (Listof String)))
 (define (interpret program)
   (run (make-state (tokenize program) '())))
 
@@ -243,18 +274,32 @@
   ;; Rebuild the file. I can't just concatenate the lines, cause 
   ;; newlines are meaningful if they occur in push blocks 
   ;; (and illegal otherwise). So the fence-post work.
-  ;; rebuild-file: input-port -> String
+  ;; rebuild-file: Input-Port -> String
   ;; Read back file at input-port
+  (: rebuild-file (-> Path-String String))
   (define (rebuild-file input-port)
     ;; FENCEPOST GARBAGE W/ ACCUM
+    (: append-items (-> (Listof String) String String))
     (define (append-items ls accum)
       (cond [(empty? ls) accum]
             [(empty? (rest ls)) (string-append accum (first ls))]
             [else (append-items (rest ls) (string-append accum
                                                          (first ls) "\n"))]))
     (append-items (call-with-input-file input-port
-                    (λ(in) (sequence->list (in-lines in))))
+                    (λ([in : Input-Port]) (sequence->list (in-lines in))))
                   ""))
+  
+  (: push-to-path-string (-> Any Path-String))
+  (define (push-to-path-string input)
+    (if (path-string? input) input
+        (error (string-append "Argument "
+                              " could not be interpreted as a "
+                              "path string."))))
+  
+  (: push-to-string (-> Any String))
+  (define (push-to-string input)
+    (if (string? input) input 
+        (error "Input " input "could not be interpreted as a string.")))
   
   ;; Sequencing for side-effects. Interpret has side-effects for reasons 
   ;; that I explained above, so it needs to be sequenced. ''begin'' can't 
@@ -270,6 +315,6 @@
                                  (read-file? #f)]
                 #:args (input)
                 (if (read-file?)
-                    (rebuild-file input)
-                    input)))
+                    (rebuild-file (push-to-path-string input))
+                    (push-to-string input))))
     (display "\n")))
